1a458df783c894f02543fd48e9bcaa272b26a1b7
Shivaji Dutta <shivaji@aurius.io>
2025-07-27 03:43:11 +0000

    sample baseline ent fr trainer

---
+++ sample_trainer.py
@@ -0,0 +1,225 @@
+import shutil
+import gc
+import os
+import time
+
+from flax import nnx
+import jax
+import jax.numpy as jnp
+import kagglehub
+import optax
+from orbax import checkpoint as ocp
+from qwix import lora
+from tunix.generate import sampler as sampler_lib
+from tunix.models.gemma import data as data_lib
+from tunix.models.gemma import gemma as gemma_lib
+from tunix.models.gemma import params as params_lib
+from tunix.sft import metrics_logger
+from tunix.sft import peft_trainer
+
+# Data
+BATCH_SIZE = 16
+
+# Model
+MESH = [(1, 8), ("fsdp", "tp")]
+# LoRA
+RANK = 16
+ALPHA = 2.0
+
+# Train
+MAX_STEPS = 100
+EVAL_EVERY_N_STEPS = 20
+NUM_EPOCHS = 3
+
+
+# Checkpoint saving
+INTERMEDIATE_CKPT_DIR = "/mnt/disks/workdir/content/intermediate_ckpt/"
+CKPT_DIR = "/mnt/disks/workdir/content/ckpts/"
+PROFILING_DIR = "/mnt/disks/workdir/content/profiling/"
+
+if "KAGGLE_USERNAME" not in os.environ or "KAGGLE_KEY" not in os.environ:
+  kagglehub.login()
+
+
+kaggle_ckpt_path = kagglehub.model_download("google/gemma/flax/2b")
+
+params = params_lib.load_and_format_params(os.path.join(kaggle_ckpt_path, "2b"))
+gemma = gemma_lib.Transformer.from_params(params, version="2b")
+checkpointer = ocp.StandardCheckpointer()
+_, state = nnx.split(gemma)
+
+checkpoint_path = os.path.join(INTERMEDIATE_CKPT_DIR, "state")
+
+# If the directory exists, remove it
+if os.path.exists(checkpoint_path):
+    print(f"Removing existing checkpoint directory: {checkpoint_path}")
+    shutil.rmtree(checkpoint_path)
+
+checkpointer.save(checkpoint_path, state)
+checkpointer.wait_until_finished()
+
+#time.sleep(60)
+
+def get_base_model(ckpt_path):
+
+  model_config = gemma_lib.TransformerConfig.gemma_2b()
+  mesh = jax.make_mesh(*MESH)
+  abs_gemma: nnx.Module = nnx.eval_shape(
+      lambda: gemma_lib.Transformer(model_config, rngs=nnx.Rngs(params=0))
+  )
+  abs_state = nnx.state(abs_gemma)
+  abs_state = jax.tree.map(
+      lambda a, s: jax.ShapeDtypeStruct(a.shape, jnp.bfloat16, sharding=s),
+      abs_state,
+      nnx.get_named_sharding(abs_state, mesh),
+  )
+  checkpointer = ocp.StandardCheckpointer()
+  restored_params = checkpointer.restore(ckpt_path, target=abs_state)
+
+  graph_def, _ = nnx.split(abs_gemma)
+  gemma = nnx.merge(graph_def, restored_params)
+  return gemma, mesh, model_config
+
+gemma, mesh, model_config = get_base_model(
+    ckpt_path=os.path.join(INTERMEDIATE_CKPT_DIR, "state")
+)
+
+gemma_tokenizer = data_lib.GemmaTokenizer(
+    os.path.join(kaggle_ckpt_path, "tokenizer.model")
+)
+
+sampler = sampler_lib.Sampler(
+    transformer=gemma,
+    tokenizer=gemma_tokenizer,
+    cache_config=sampler_lib.CacheConfig(
+        cache_size=256,
+        num_layers=model_config.num_layers,
+        num_kv_heads=model_config.num_kv_heads,
+        head_dim=model_config.head_dim,
+    ),
+)
+
+input_batch = [
+    "Translate this into French:\nHello, my name is Morgane.\n",
+    "Translate this into French:\nThis dish is delicious!\n",
+    "Translate this into French:\nI am a student.\n",
+    "Translate this into French:\nHow's the weather today?\n",
+]
+
+out_data = sampler(
+    input_strings=input_batch,
+    total_generation_steps=10,  # The number of steps performed when generating a response.
+)
+
+for input_string, out_string in zip(input_batch, out_data.text):
+  print(f"----------------------")
+  print(f"Prompt:\n{input_string}")
+  print(f"Output:\n{out_string}")
+
+
+def get_lora_model(base_model, mesh):
+  lora_provider = lora.LoraProvider(
+      module_path=".*q_einsum|.*kv_einsum|.*gate_proj|.*down_proj|.*up_proj",
+      rank=RANK,
+      alpha=ALPHA,
+      # comment the two args below for LoRA (w/o quantisation).
+      weight_qtype="nf4",
+      tile_size=256,
+  )
+
+  model_input = base_model.get_model_input()
+  lora_model = lora.apply_lora_to_model(
+      base_model, lora_provider, **model_input
+  )
+
+  with mesh:
+    state = nnx.state(lora_model)
+    pspecs = nnx.get_partition_spec(state)
+    sharded_state = jax.lax.with_sharding_constraint(state, pspecs)
+    nnx.update(lora_model, sharded_state)
+
+  return lora_model
+
+lora_gemma = get_lora_model(gemma, mesh=mesh)
+
+
+# Loads the training and validation datasets
+train_ds, validation_ds = data_lib.create_datasets(
+    dataset_name='mtnt/en-fr',
+    # Uncomment the line below to use a Hugging Face dataset.
+    # Note that this requires upgrading the 'datasets' package and restarting
+    # the Colab runtime.
+    # dataset_name='Helsinki-NLP/opus-100',
+    global_batch_size=BATCH_SIZE,
+    max_target_length=256,
+    num_train_epochs=NUM_EPOCHS,
+    tokenizer=gemma_tokenizer,
+)
+
+
+def gen_model_input_fn(x: peft_trainer.TrainingInput):
+  pad_mask = x.input_tokens != gemma_tokenizer.pad_id()
+  positions = gemma_lib.build_positions_from_mask(pad_mask)
+  attention_mask = gemma_lib.make_causal_attn_mask(pad_mask)
+  return {
+      'input_tokens': x.input_tokens,
+      'input_mask': x.input_mask,
+      'positions': positions,
+      'attention_mask': attention_mask,
+  }
+
+
+
+logging_option = metrics_logger.MetricsLoggerOptions(
+    log_dir="/mnt/disks/workdir/content/metrics_logger", flush_every_n_steps=20
+)
+
+
+training_config = peft_trainer.TrainingConfig(
+    eval_every_n_steps=EVAL_EVERY_N_STEPS,
+    max_steps=MAX_STEPS,
+    checkpoint_root_directory=CKPT_DIR,
+)
+lora_trainer = peft_trainer.PeftTrainer(
+    lora_gemma, optax.adamw(1e-3), training_config
+).with_gen_model_input_fn(gen_model_input_fn)
+
+#with jax.profiler.trace(os.path.join(PROFILING_DIR, "peft")):
+with mesh:
+    lora_trainer.train(train_ds, validation_ds)
+
+gemma_tokenizer = data_lib.GemmaTokenizer(
+    os.path.join(kaggle_ckpt_path, "tokenizer.model")
+)
+
+sampler = sampler_lib.Sampler(
+    transformer=lora_gemma,
+    tokenizer=gemma_tokenizer,
+    cache_config=sampler_lib.CacheConfig(
+        cache_size=256,
+        num_layers=model_config.num_layers,
+        num_kv_heads=model_config.num_kv_heads,
+        head_dim=model_config.head_dim,
+    ),
+)
+
+input_batch = [
+    "Translate this into French:\nHello, my name is Morgane.\n",
+    "Translate this into French:\nThis dish is delicious!\n",
+    "Translate this into French:\nI am a student.\n",
+    "Translate this into French:\nHow's the weather today?\n",
+]
+
+out_data = sampler(
+    input_strings=input_batch,
+    total_generation_steps=10,  # The number of steps performed when generating a response.
+)
+
+for input_string, out_string in zip(input_batch, out_data.text):
+  print(f"----------------------")
+  print(f"Prompt:\n{input_string}")
+  print(f"Output:\n{out_string}")
+
+
+
+
